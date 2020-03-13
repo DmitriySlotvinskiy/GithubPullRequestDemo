b85243a9a1a4f6731ae9a95c341e4b1f52614baf
Dmitriy Slotvinskiy <slotvinskiy@gamil.com>
2020-03-13 13:18:56 +0200

    Init commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,31 @@
 package com.company;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.OptionalDouble;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Main {
 
     public static void main(String[] args) {
-        // show me you Stream API power
+
+        //Входящий параметр: отсортированный массив типа int который содержит отрицательные и положительные значения.
+//Вернуть отсортированный массив который состоит из квадратов значений.
+//Input: [-4,-1,0,3,10]
+//Output: [0,1,9,16,100]
+
+        int[] arr = {-4,-1,0,3,10};
+        int[] newArr = getSortedPow(arr);
+        System.out.println(Arrays.toString(newArr));
         System.out.println("test");
+    }
+
+    private static int[] getSortedPow(int[] arr) {
+        return Arrays.stream(arr)
+                .map((x)-> x * x)
+                .sorted()
+                .toArray();
     }
 
 }
